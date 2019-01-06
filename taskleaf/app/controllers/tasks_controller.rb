2a1cf6da2f39bb588795367127ea70efdbc61b53
howdy39 <tatsuya.howdy@gmail.com>
2019-01-06 21:44:08 +0900

    fix: 検証エラーに対応

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -12,9 +12,12 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。" # redirect_to　@task でタスクの詳細画面に遷移
+    else
+      render :new
+    end
   end
 
   def edit
